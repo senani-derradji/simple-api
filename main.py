a13b7086d79497e78b261fa5352a08ab6dd49baa
Derradji Senani <89805807+senani-derradji@users.noreply.github.com>
2025-10-03 00:54:17 +0100

    Update main.py (added dbs)

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+from models import User
+from sqlalchemy.orm import sessionmaker
+from db import engine
 from fastapi.middleware.cors import CORSMiddleware
+
+session_local = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
 app = FastAPI()
 
@@ -12,40 +17,50 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-students = [
-        {"name": "Alice", "age": 20, "grade": "A", "city": "New York"},
-        {"name": "Bob", "age": 22, "grade": "B", "city": "Los Angeles"},
-        {"name": "Charlie", "age": 23, "grade": "C", "city": "Chicago"},
-        {"name": "David", "age": 21, "grade": "B", "city": "Houston"},
-        {"name": "Eva", "age": 24, "grade": "A", "city": "Phoenix"},]
-
 class STUDENT(BaseModel):
     name: str
     age: int
     grade: str
-    city: str
 
 @app.get("/students/")
 def get_students():
-    return students
+    session = session_local()
+    students = session.query(User).all()
+    session.close()
+    return {"students": students}
+
 
 @app.post("/students/")
 def add_student(student: STUDENT):
-    students.append(student)
-    return {"message": "Student added successfully"}
+    session = session_local()
+    new_student = User(name=student.name, age=student.age, grade=student.grade)
+    session.add(new_student)
+    session.commit()
+    session.close()
+    return {"message": f"{new_student.name} added successfully"}
 
-@app.put("/students/{age}")
-def update_student(s_name: str, student: STUDENT):
-    for s in students:
-        if s['name'] == s_name:
-            s.update(student.dict())
-            return {"message": "Student updated successfully"}
-    return {"message": "Student not found"}
+@app.put("/students/{student_id}")
+def update_student(student_id: int, student: STUDENT):
+    session = session_local()
+    db_student = session.query(User).filter(User.id == student_id).first()
+    if not db_student:
+        session.close()
+        raise HTTPException(status_code=404, detail=f"Student {db_student} not found")
+    db_student.name = student.name
+    db_student.age = student.age
+    db_student.grade = student.grade
+    session.commit()
+    session.close()
+    return {"message": "Student {db_student.name} has been updated successfully"}
 
-@app.delete("/students/{age}")
-def delete_student(std_age: int):
-    for s in students:
-        if s["age"] == std_age:
-            students.remove(s)
-            return {"message": "Student has been deleted"}
-    return {"error": "This student not found !!"}
+@app.delete("/students/{student_id}")
+def delete_student(student_id: int):
+    session = session_local()
+    db_student = session.query(User).filter(User.id == student_id).first()
+    if not db_student:
+        session.close()
+        raise HTTPException(status_code=404, detail=f"Student {db_student} not found")
+    session.delete(db_student)
+    session.commit()
+    session.close()
+    return {"message": f"Student {db_student.name} has been deleted successfully"}
